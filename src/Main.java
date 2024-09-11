47f3c66d6164b8f912121173836be703a27f2230
Kania47 <skilllu11@gmail.com>
2024-09-11 14:02:59 +0200

    Kolekcje1

---
+++ src/Main.java
@@ -0,0 +1,32 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Laczenie z githubem!!!!!!
+//1. git init
+//2. git config user.name "nazwa"
+//3. git config user.email "email"
+//4. git add .
+//5. git commit -m "nazwa commita"
+//6. git remote add origin "link do repo"
+//7. git push -u origin master
+//8. git push
